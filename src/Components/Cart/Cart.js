d0d84f4d28a51653b34e8d6ee09607b2217b4b3b
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2022-12-07 21:39:48 +0600

    Local Storage Part

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,8 +7,11 @@ const Orders = ({ cart }) => {
   // Direct {Cart} Ke Props Hisebe Receive Korteche
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
+
   for (const product of cart) {
-    total = total + product.price;
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
   const tax = parseFloat((total * 0.1).toFixed(2));
@@ -16,7 +19,7 @@ const Orders = ({ cart }) => {
   return (
     <div className="cart">
       <h3>Order Summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping: ${shipping} </p>
       <p>Tax: ${tax}</p>
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import "./Shop.css";
 
 const Shop = () => {
@@ -11,10 +12,37 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
-  const handleAddToCart = (product) => {
-    console.log(product);
-    const newCart = [...cart, product];
+  // Data Load Korte useEffect Use Hoy.
+  // Object Ke For In Loop Marte Hoy.
+  // FIlter Onekgular Jonne.
+  // Find Nirdisto Ektar Jonne.
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
+  const handleAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exist = cart.find((product) => product.id === selectedProduct.id);
+    if (!exist) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exist.quantity = exist.quantity + 1;
+      newCart = [...rest, exist];
+    }
     setCart(newCart);
+    addToDb(selectedProduct.id);
     // ...Cart Mane Existing Array Te Onno Ekta Element Copy Kore Add Kora
     // State Jekhane Eventhandler Sekhane.
   };
